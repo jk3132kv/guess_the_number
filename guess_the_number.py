a88e771c0b836cb0dfab1de7a91f172a7d96cc60
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-01-26 09:51:55 -0600

    add while loop so user can try again if inavlid guess

--- guess_the_number.py
+++ guess_the_number.py
@@ -16,10 +16,11 @@ def generate_secret(low, high):
 
 
 def get_guess():
-    """ get user's guess, as an integer number """
-    try:
-        return int(input('Guess the secret number? '))
-    except: print('that is not a number')    
+    """ get user's guess, as an integer number. Ensures user's guess is an int. """
+    while True:
+        try:
+            return int(input('Guess the secret number? '))
+        except: print('That is not a number. Please try again.')    
 
 
 def check_guess(guess, secret):
